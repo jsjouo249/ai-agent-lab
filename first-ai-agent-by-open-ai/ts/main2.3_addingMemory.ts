932c6bfd7c398980912284f683337ab5e78dbf3d
조성준 <43127287+jsjouo249@users.noreply.github.com>
2025-12-12 16:21:49 +0900

    add/ai-agent_2.3 (#3)

--- first-ai-agent-by-open-ai/ts/main2.3_addingMemory.ts
+++ first-ai-agent-by-open-ai/ts/main2.3_addingMemory.ts
@@ -16,31 +16,6 @@ async function callAI(): Promise<void> {
     model: "gpt-4o-mini",
     messages: messages,
   });
-const PROMPT = `
-I HAVE the following functions in my system.
-
-\`get_weather\`
-\`get_currency\`
-\`get_news\`
-
-ALL of them receive the name of a country as an argument. (i.e get_news('Spain'))
-
-Please answer with the name of the function that you would like me to run.
-
-PLeare say nothing else, junst the name of the function with the arguments.
-
-Answer the following question:
-
-What is the weather in Greece?
-`;
-
-async function main() {
-  const response = await client.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [{ role: "user", content: PROMPT }],
-  });
-
-  console.log("Response:", response);
 
   const message = response.choices[0].message.content;
   messages.push({ role: "assistant", content: message });
